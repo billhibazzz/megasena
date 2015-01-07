f963cbbb3b45c8a05c82c35200f094b3c07e534e
Naner Nunes <naner@naner.com.br>
2015-01-07 00:34:21 -0200

    Removed additional spaces and added .gitignore

--- test/models/megasena_test.rb
+++ test/models/megasena_test.rb
@@ -3,20 +3,21 @@ require_relative '../test_helper'
 describe Lottery::MegaSena do
 
   before do
-	  @megasena = Lottery::MegaSena.new
+    @megasena = Lottery::MegaSena.new
   end
 
   describe "Should return values from CAIXA" do
+
     it "should return a contest number" do
       assert_instance_of Fixnum, @megasena.contest
-		end
+    end
 
-		it "should return an array with the result" do
+    it "should return an array with the result" do
       assert_instance_of Array,  @megasena.numbers
-			assert_equal       6,      @megasena.numbers.size
-			assert_equal       1,      @megasena.numbers.map(&:class).uniq.size
-		end
+      assert_equal       6,      @megasena.numbers.size
+      assert_equal       1,      @megasena.numbers.map(&:class).uniq.size
+    end
 
-	end
+  end
 
 end
--- test/models/quina_test.rb
+++ test/models/quina_test.rb
@@ -3,20 +3,21 @@ require_relative '../test_helper'
 describe Lottery::Quina do
 
   before do
-	  @quina = Lottery::Quina.new
+    @quina = Lottery::Quina.new
   end
 
   describe "Should return values from CAIXA" do
+
     it "should return a contest number" do
       assert_instance_of Fixnum, @quina.contest
-		end
+    end
 
-		it "should return an array with the result" do
+    it "should return an array with the result" do
       assert_instance_of Array,  @quina.numbers
-			assert_equal       5,      @quina.numbers.size
-			assert_equal       1,      @quina.numbers.map(&:class).uniq.size
-		end
+      assert_equal       5,      @quina.numbers.size
+      assert_equal       1,      @quina.numbers.map(&:class).uniq.size
+    end
 
-	end
+  end
 
 end
