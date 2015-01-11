1440316522cb20bb68b719e01598b51765b04443
Naner Nunes <naner@naner.com.br>
2015-01-11 04:25:48 -0200

    Improve CodeQuality inside class Loteca

--- lib/lottery/loteca.rb
+++ lib/lottery/loteca.rb
@@ -3,13 +3,14 @@ module Lottery
     def get_numbers
 
       games = Hash.new
+      dom   = path[:etc]
 
       super.each do |tr|
         games.merge!({
-          tr.xpath(path[:etc][:game]).text => {
-            :date     => tr.xpath(path[:etc][:date]).text,
-            :player1  => { :name => tr.xpath(path[:etc][:player1]).text, :score => tr.xpath(path[:etc][:score_1]).text.to_i },
-            :player2  => { :name => tr.xpath(path[:etc][:player2]).text, :score => tr.xpath(path[:etc][:score_2]).text.to_i }
+          tr.xpath(dom[:game]).text => {
+            :date     => tr.xpath(dom[:date]).text,
+            :player1  => { :name => tr.xpath(dom[:player1]).text, :score => tr.xpath(dom[:score_1]).text.to_i },
+            :player2  => { :name => tr.xpath(dom[:player2]).text, :score => tr.xpath(dom[:score_2]).text.to_i }
           }
         })
       end
